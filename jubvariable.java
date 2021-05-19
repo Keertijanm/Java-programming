688efcf209d13ab1b4c930cfc20868a4657e25e7
Keertijanm <81510729+Keertijanm@users.noreply.github.com>
2021-05-19 20:35:18 +0530

    Add files via upload
    I am starting java from basic  .  so I upload  file in  series  , to help a new coder to learn and practice java . 
    #java :)

---
+++ jubvariable.java
@@ -0,0 +1,24 @@
+public class jubvariable {
+    public  static void main(String[] args){
+        int avalue = 42;
+        short ashort = 93;
+        long along = 8304;
+        double aDouble = 3.4553;
+        float  afloat = 5.235f;
+        char achar = 'b';
+        Boolean aBoolean = true ;
+        byte  abyte = 127;
+        System.out.println(avalue);
+        System.out.println(ashort);
+        System.out.println(along);
+        System.out.println(aDouble);
+        System.out.println(afloat);
+        System.out.println(achar);
+        System.out.println(aBoolean);
+        System.out.println(abyte);
+        System.out.println("variable type introduction complete");
+     
+
+
+    }  
+}
---
+++ jucstring.java
@@ -0,0 +1,11 @@
+public class jucstring {
+     public static void main(String[] args) {
+        int anumber =  32;
+        double aDouble = 9.4;
+        String t1 = " ";
+        String text = "ABES";
+        System.out.println("College" + t1 + text  + "code is"+ anumber );
+
+    }     
+    
+}
